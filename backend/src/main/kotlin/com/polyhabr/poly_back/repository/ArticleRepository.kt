37718bf5a827c58780bd4fc3c052bb5c11bd7dc2
gmescheryakov <mescheryakov9201@gmail.com>
2023-02-23 15:15:00 +0300

    AR-1011 temp commit

--- backend/src/main/kotlin/com/polyhabr/poly_back/repository/ArticleRepository.kt
+++ backend/src/main/kotlin/com/polyhabr/poly_back/repository/ArticleRepository.kt
@@ -3,11 +3,12 @@ package com.polyhabr.poly_back.repository
 import com.polyhabr.poly_back.entity.Article
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
 interface ArticleRepository: JpaRepository<Article, Long>{
     @Query("SELECT a FROM Article a WHERE a.previewText LIKE %:pname%")
-    fun findArticleByName(pageable: PageRequest, @Param("pname") pname: String): Page<Article>
+    fun findArticleByName(pageable: Pageable, @Param("pname") pname: String): Page<Article>
 }
