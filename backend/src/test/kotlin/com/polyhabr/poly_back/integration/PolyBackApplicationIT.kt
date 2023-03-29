6013e885ec3b2f59d18b7776c27d0a689de88d05
Shabinsky Dmitry <sdimosikvip@gmail.com>
2023-03-30 02:29:05 +0300

    ci config update 3

--- backend/src/test/kotlin/com/polyhabr/poly_back/integration/PolyBackApplicationIT.kt
+++ backend/src/test/kotlin/com/polyhabr/poly_back/integration/PolyBackApplicationIT.kt
@@ -2,31 +2,33 @@ package com.polyhabr.poly_back.integration
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
 @SpringBootTest
 class PolyBackApplicationIT {
 
-//    companion object {
-//        @Container
-//        val container = PostgreSQLContainer<Nothing>("postgres:12").apply {
-//            withDatabaseName("postgres")
-//            withUsername("postgres")
-//            withPassword("postgres")
-//        }
-//
-//        @JvmStatic
-//        @DynamicPropertySource
-//        fun properties(registry: DynamicPropertyRegistry) {
-//            registry.add("spring.datasource.url", container::getJdbcUrl);
-//            registry.add("spring.datasource.password", container::getPassword);
-//            registry.add("spring.datasource.username", container::getUsername);
-//        }
-//    }
+    companion object {
+        @Container
+        val container = PostgreSQLContainer<Nothing>("postgres:12").apply {
+            withDatabaseName("postgres")
+            withUsername("postgres")
+            withPassword("postgres")
+        }
 
-    @Test
-    fun contextLoads() {
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", container::getJdbcUrl);
+            registry.add("spring.datasource.password", container::getPassword);
+            registry.add("spring.datasource.username", container::getUsername);
+        }
     }
 
+    @Test
+    fun contextLoads() {}
 }
