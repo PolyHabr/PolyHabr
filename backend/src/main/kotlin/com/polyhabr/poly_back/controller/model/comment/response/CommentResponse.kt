c530c786aa51377b0026230d20ca5816697e7c37
Shabinsky Dmitry <sdimosikvip@gmail.com>
2023-05-09 16:43:30 +0300

    tmp commit

--- backend/src/main/kotlin/com/polyhabr/poly_back/controller/model/comment/response/CommentResponse.kt
+++ backend/src/main/kotlin/com/polyhabr/poly_back/controller/model/comment/response/CommentResponse.kt
@@ -1,16 +1,19 @@
 package com.polyhabr.poly_back.controller.model.comment.response
 
 import com.polyhabr.poly_back.controller.model.article.response.toResponse
+import com.polyhabr.poly_back.controller.model.user.response.UserOtherResponse
+import com.polyhabr.poly_back.controller.model.user.response.toOtherResponse
 import com.polyhabr.poly_back.dto.CommentDto
 import com.polyhabr.poly_back.entity.Article
 import com.polyhabr.poly_back.entity.auth.User
+import com.polyhabr.poly_back.entity.auth.toDtoWithoutPasswordAndEmail
 import com.polyhabr.poly_back.utility.DateTimeUtils
 
 data class CommentResponse(
     val id: Long,
     val text: String,
-    val articleId: Article,
-    val userId: User,
+    val articleId: Long,
+    val userId: UserOtherResponse,
     val data: String
 )
 
@@ -18,8 +21,8 @@ fun CommentDto.toResponse(): CommentResponse {
     return CommentResponse(
         id = this.id!!,
         text = this.text!!,
-        articleId = this.articleId!!,
-        userId = this.userId!!,
+        articleId = this.articleId!!.id!!,
+        userId = this.userId!!.toDtoWithoutPasswordAndEmail().toOtherResponse(),
         data = this.data!!.toString(DateTimeUtils.defaultFormat)
     )
 }
